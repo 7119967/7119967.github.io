f3c9979712df6dc572c0f4cd3a5fab9fcc5c7c49
Nurzhanov Rashit <7119967@mail.ru>
2019-08-30 12:48:35 +0600

    first commit

---
+++ ComplianceBotABK-src/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild EchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=compliancebotabk-Web-Deploy.pubxml -p:Password=eNYN21z9BetrsoA7qK9Kj5iLNZpuTAMS9c19JcHeS3nxcfNe0YXjoEuufsAJ
+
